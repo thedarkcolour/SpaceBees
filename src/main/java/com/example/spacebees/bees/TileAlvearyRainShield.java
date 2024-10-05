33ebf0694e35382dcdfcb3bd1bc44251da0cd6a4
jasonsherrmann <68131989+jasonsherrmann@users.noreply.github.com>
2024-10-04 22:28:29 -0400

    Added logging for rainshield
    Added logging to rain shield to try to diagnose why it wont work on save/load

--- src/main/java/com/example/spacebees/bees/TileAlvearyRainShield.java
+++ src/main/java/com/example/spacebees/bees/TileAlvearyRainShield.java
@@ -1,33 +1,35 @@
 package com.example.spacebees.bees;
 
-import org.jetbrains.annotations.ApiStatus.OverrideOnly;
 
 import forestry.api.apiculture.IBeeModifier;
-import forestry.api.climate.IClimateControlled;
 import forestry.api.multiblock.IAlvearyComponent;
 import forestry.apiculture.blocks.BlockAlvearyType;
 import forestry.apiculture.multiblock.TileAlveary;
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.state.BlockState;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
 
 public class TileAlvearyRainShield extends TileAlveary implements IAlvearyComponent.BeeModifier {
-	
+	private static final Logger LOGGER = LogUtils.getLogger();
 	private static final IBeeModifier MODIFIER = new IBeeModifier() {
 		@Override
 		public boolean isSealed() {
+			LOGGER.info("IBeeModifier Modifier isSealed sent True");
 			return true;
+			
 		}
 	};
-
-	
     public TileAlvearyRainShield(BlockPos pos, BlockState state) {
         super(BlockAlvearyType.PLAIN, pos, state);
+		
     }
 	@Override
 	public IBeeModifier getBeeModifier() {
+		LOGGER.info("getBeeModifer sent isSealed sent True");
+		MODIFIER.isSealed();
 		return MODIFIER;
 	}
-
 
    }
